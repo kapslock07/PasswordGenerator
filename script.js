b98049b32f8b0871c5594ac17bcb5859062bb875
Ryan Kaplan <ryankaplan7@gmail.com>
2020-01-09 15:05:26 -0500

    started adding code to js

--- script.js
+++ script.js
@@ -3,10 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var characterNum = prompt("Choose how many characters will be in your password. Ther must be at least 8 and no more than 128.");
 
-  passwordText.value = password;
+  alert("You have chosen " + characterNum + " characters");
+
+
+  var characterTypes = prompt("Choose what type of characters will be in your password. Please choose either 'lowercase', 'uppercase', 'numeric', 'special characters', 'all', 'lu' for lowercase and uppercase, 'ln' for lowercase and numeric, 'ls' for lowercase and special characters, 'un' for uppercase and numeric, 'us' for uppercase and special characters, 'ns' for numeric and special characters.");
+  // var lu = "lowercase and uppercase";
+  // var ln = "lowercase and numeric";
+  // var ls = "lowercase and special characters";
+  // var un = "uppercase and numeric";
+  // var us = "uppercase and special characters";
+  // var ns = "numeric and special characters";
+
+  alert("You have chosen " + characterTypes);
+
+  // THIS WAS PROVIDED
+  // function writePassword() {
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
